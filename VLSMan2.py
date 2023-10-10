379dd5dd5dffc06bb59ee4d41cc303b5fe3f9c96
Ryan <BhuiyanRyan101@gmail.com>
2023-10-10 08:18:09 -0500

    Update VLSMan2.py

---
+++ VLSMan2.py
@@ -0,0 +1,16 @@
+#Begin Program
+
+#Initalization Complete
+print("System Initalized, Welcome To VLSMan 2.1")
+print("////////////////////////////////////////////////////////////")
+
+#Main Code, will keep running until break.
+while True:
+
+    #Program needs to take input from user: HostsNeeded;
+    hostsNeeded = int(input("Enter a numerical value below:\n"))
+
+    #LOGIC ENGINE ----------------
+    print("Value was " + str(hostsNeeded))
+
+    continue
